b9aa58a3d3b0aa8ebe64b1f4a84b46f76e46cff7
Josh Ault <josh.ault3@gmail.com>
2017-02-24 13:53:31 -0600

    public settings route

--- main.go
+++ main.go
@@ -1,67 +1,96 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+	"sync"
 	"time"
 )
 
 var logger = log.New(os.Stdout, "healthcheck-app", log.LstdFlags)
 
-type handler struct{}
-type adminHandler struct{}
+type settingsHandler struct{}
+type rootHandler struct{}
 
-var (
-	healthcheckPassing = true
-	duration           int
-)
+type Settings struct {
+	SleepDuration int  `json:"sleepDuration"`
+	Healthy       bool `json:"healthy"`
+}
+
+var mutex sync.RWMutex
+var settings *Settings
 
 func main() {
-	duration, _ = strconv.Atoi(os.Getenv("SLEEP_DURATION"))
-	go adminServer()
-	h := &handler{}
+	settings.SleepDuration = 0
+	settings.Healthy = true
+	http.Handle("/settings", &settingsHandler{})
+	http.Handle("/", &rootHandler{})
 	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: h,
+		Addr: ":" + os.Getenv("PORT"),
 	}
 	log.Fatal(srv.ListenAndServe())
 }
 
-func adminServer() {
-	h := &adminHandler{}
-	srv := &http.Server{
-		Addr:    ":8081",
-		Handler: h,
-	}
-	log.Fatal(srv.ListenAndServe())
-}
-
-func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *rootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
+	if r.Method != http.MethodGet {
+		w.WriteHeader(405)
+		return
+	}
+	mutex.RLock()
+	healthy := settings.Healthy
+	duration := settings.SleepDuration
+	mutex.RUnlock()
+
 	time.Sleep(time.Duration(duration) * time.Second)
-	if healthcheckPassing {
+	str := "passing"
+	if healthy {
 		w.WriteHeader(200)
 	} else {
-		w.WriteHeader(404)
-	}
-	str := "passing"
-	if !healthcheckPassing {
 		str = "failing"
+		w.WriteHeader(500)
 	}
 	logger.Printf("Received request: %s %s - %s\n", r.Method, r.RequestURI, str)
 	w.Write([]byte(str + "-" + os.Getenv("CATALYZE_JOB_ID")))
 }
 
-func (h *adminHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *settingsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	healthcheckPassing = r.Method != http.MethodDelete
-	str := "passing"
-	if !healthcheckPassing {
-		str = "failing"
+	if r.Method != http.MethodGet && r.Method != http.MethodPost {
+		w.WriteHeader(405)
+		return
 	}
-	logger.Printf("Received admin request: %s %s - healthcheck is now %s\n", r.Method, r.RequestURI, str)
-	w.WriteHeader(200)
-	w.Write([]byte(str))
+	logger.Printf("Received admin request: %s %s\n", r.Method, r.RequestURI)
+	if r.Method == http.MethodGet {
+		mutex.RLock()
+		defer mutex.RUnlock()
+		b, _ := json.Marshal(settings)
+		w.WriteHeader(200)
+		w.Write(b)
+	} else if r.Method == http.MethodPost {
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+		}
+
+		var tempSettings *Settings
+		err = json.Unmarshal(data, tempSettings)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+		}
+
+		mutex.Lock()
+		defer mutex.Unlock()
+		settings.Healthy = tempSettings.Healthy
+		settings.SleepDuration = tempSettings.SleepDuration
+
+		b, _ := json.Marshal(settings)
+		w.WriteHeader(200)
+		w.Write(b)
+	}
 }
