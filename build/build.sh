472b360a264cc77e2ed711c1d52ebb505a54e39a
bhperry <bhperry94@gmail.com>
2018-01-02 17:18:05 -0600

    change tag to 2.0

--- build/build.sh
+++ build/build.sh
@@ -3,8 +3,9 @@ rm -f root/bin/healthcheckapp
 go build -o root/bin/healthcheckapp ../main.go
 rm -f root.tar
 (cd root && fakeroot tar cvf ../root.tar .)
-image=registry-sbox05.datica.com/${namespace}/healthcheckapp
-tag=1.0
+namespace=datica
+image=registry-sbox05.datica.com/${namespace}/healthcheck-app
+tag=2.0
 
 docker build -t ${image}:${tag} .
 
