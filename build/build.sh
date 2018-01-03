a6603c39a2b7905fca1d883dea47cd8af84f701e
bhperry <bhperry94@gmail.com>
2018-01-03 14:20:08 -0600

    Add params to build.sh, set target architecture for golang to run in ubuntu container

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
+if [ "$#" == "3" ]; then
+    REGISTRY=$1
+    NAMESPACE=$2
+    TAG=$3
+fi
+
 rm -f root/bin/healthcheckapp
-go build -o root/bin/healthcheckapp ../main.go
+GOOS=linux GOARCH=amd64 go build -o root/bin/healthcheckapp ../main.go
 (cd root && fakeroot tar cvf ../root.tar .)
 image=${REGISTRY}/${NAMESPACE}/healthcheckapp
 
